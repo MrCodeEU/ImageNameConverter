6bfc0f33d483319d689332fa952bfc0a0adb983e
Michiii2406 <43099483+Michiii2406@users.noreply.github.com>
2021-02-09 19:09:00 +0100

    Projektdateien hinzufügen.

---
+++ ImageNameConverter/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ImageNameConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.lstOld = new System.Windows.Forms.ListBox();
+            this.lstNew = new System.Windows.Forms.ListBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.picSelected = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelected)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(91, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Dateien öffnen";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(109, 12);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(91, 23);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Ordner öffnen";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRename.Location = new System.Drawing.Point(641, 327);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 2;
+            this.btnRename.Text = "Umbennen";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.BtnRename_Click);
+            // 
+            // lstOld
+            // 
+            this.lstOld.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstOld.FormattingEnabled = true;
+            this.lstOld.Location = new System.Drawing.Point(12, 41);
+            this.lstOld.Name = "lstOld";
+            this.lstOld.Size = new System.Drawing.Size(490, 147);
+            this.lstOld.TabIndex = 3;
+            this.lstOld.SelectedIndexChanged += new System.EventHandler(this.LstOld_SelectedIndexChanged);
+            // 
+            // lstNew
+            // 
+            this.lstNew.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstNew.FormattingEnabled = true;
+            this.lstNew.Location = new System.Drawing.Point(12, 203);
+            this.lstNew.Name = "lstNew";
+            this.lstNew.Size = new System.Drawing.Size(490, 147);
+            this.lstNew.TabIndex = 4;
+            this.lstNew.SelectedIndexChanged += new System.EventHandler(this.LstNew_SelectedIndexChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // picSelected
+            // 
+            this.picSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picSelected.Location = new System.Drawing.Point(508, 12);
+            this.picSelected.Name = "picSelected";
+            this.picSelected.Size = new System.Drawing.Size(208, 156);
+            this.picSelected.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSelected.TabIndex = 5;
+            this.picSelected.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 362);
+            this.Controls.Add(this.picSelected);
+            this.Controls.Add(this.lstNew);
+            this.Controls.Add(this.lstOld);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Bilddateienname Umwandler by MrCode";
+            ((System.ComponentModel.ISupportInitialize)(this.picSelected)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.ListBox lstOld;
+        private System.Windows.Forms.ListBox lstNew;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.PictureBox picSelected;
+    }
+}
+
---
+++ ImageNameConverter/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ImageNameConverter
+{
+    public partial class Form1 : Form
+    {
+        string filePath = string.Empty;
+        string[] files = new string[1];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            //Initialize openFiledialog
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Bild Dateien (*.BMP; *.JPG; *.JPEG; *.NEF; *.HEIC; *.MOV)|*.BMP;*.JPG;*.JPEG;*.NEF;*.HEIC;*.MOV";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = true;
+
+            //See if something was selected
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Delete previos Files
+                lstOld.Items.Clear();
+
+                //Get the path of specified file
+                filePath = openFileDialog.FileNames[0].Replace(openFileDialog.SafeFileNames[0], "");
+                Array.Resize<string>(ref files, openFileDialog.FileNames.Length);
+                files = openFileDialog.FileNames;
+
+                //Set selected Image as Image of PictureBox
+                try
+                {
+                    //Set selected Image as Image of PictureBox
+                    picSelected.Image = ChangeRotation(Image.FromFile(files[0]));
+                }
+                catch (Exception)
+                {
+                    picSelected.Image = picSelected.ErrorImage;
+                }
+                lstNew.SelectedIndex = lstOld.SelectedIndex;
+
+                //Shpow filepaths in listbox
+                lstOld.Items.AddRange(files);
+
+                //Generate new File Names and show them in listbox
+                lstNew.Items.AddRange(Umbennen(files));
+
+
+            }
+        }
+
+        //Ändert die Rotation der Bilder aufgrund der EXIF Daten.
+        public Image ChangeRotation(Image image)
+        {
+
+            if (Array.IndexOf(image.PropertyIdList, 0x0112) > -1)
+            {
+                int orientation;
+
+                orientation = image.GetPropertyItem(0x0112).Value[0];
+
+                if (orientation >= 1 && orientation <= 8)
+                {
+                    switch (orientation)
+                    {
+                        case 2:
+                            image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                            break;
+                        case 3:
+                            image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                            break;
+                        case 4:
+                            image.RotateFlip(RotateFlipType.Rotate180FlipX);
+                            break;
+                        case 5:
+                            image.RotateFlip(RotateFlipType.Rotate90FlipX);
+                            break;
+                        case 6:
+                            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            break;
+                        case 7:
+                            image.RotateFlip(RotateFlipType.Rotate270FlipX);
+                            break;
+                        case 8:
+                            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            break;
+                    }
+
+                }
+            }
+            return image;
+        }
+
+        private void LstOld_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //Set selected Image as Image of PictureBox
+                picSelected.Image = ChangeRotation(Image.FromFile(files[lstOld.SelectedIndex]));
+            }
+            catch (Exception)
+            {
+                picSelected.Image = picSelected.ErrorImage;
+            }
+            lstNew.SelectedIndex = lstOld.SelectedIndex;
+        }
+
+        private string[] Umbennen(string[] Dateien)
+        {
+            string[] newDateien = new string[Dateien.Length];
+
+            for (int i = 0; i < Dateien.Length; i++)
+            {
+                
+                if (!Dateien[i].Contains("Screenshot"))
+                {
+                    if (!Dateien[i].Contains("HEIC") && !Dateien[i].Contains("heic") && !Dateien[i].Contains("MOV") && !Dateien[i].Contains("mov"))
+                    {
+                        FileStream fs = new FileStream(Dateien[i], FileMode.Open, FileAccess.Read);
+                        Image myImage = Image.FromStream(fs, false, false);
+
+                        PropertyItem propItem = myImage.PropertyItems[0];
+
+                        try
+                        {
+                            propItem = myImage.GetPropertyItem(36867);
+                        }
+                        catch (Exception)
+                        {
+                            propItem = null;
+                        }
+
+                        myImage.Dispose();
+
+                        if (propItem != null)
+                        {
+                            string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+
+                            //Convert Name captured.Year + "_" + captured.Month + "_" + captured.day + " " + captured.Hour + "-" + captured.Minute + "-" + captured.Second;
+                            newDateien[i] = filePath + DateTime.Parse(dateTaken).ToString("s").Replace("-", "_").Replace("T", " ").Replace(":", "-") + "." + Dateien[i].Split('.')[1];
+                        }
+                        else
+                        {
+                            //Kann Datei nicht umbennen, da sie kein Aufnahmnedatum in EXIF gespeichert hat
+                            newDateien[i] = Dateien[i];
+                        }
+                    }
+                    else
+                    {
+                        //HEIC und MOV Dateien umbennen herausfinden
+                        newDateien[i] = Dateien[i];
+                    }
+                }
+                else
+                {
+                    //Screenshot umbennen
+                    newDateien[i] = Dateien[i];
+                }
+            }
+
+            return newDateien;
+        }
+
+        //we init this once so that if the function is repeatedly called
+        //it isn't stressing the garbage man
+        private static readonly Regex r = new Regex(":");
+
+        private void LstNew_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstOld.SelectedIndex = lstNew.SelectedIndex;
+        }
+
+        private void BtnRename_Click(object sender, EventArgs e)
+        {
+            const string message = "Wollen sie die ausgewählten Dateien sicher umbennen?";
+            const string caption = "Dateien Umbennen";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            picSelected.Image.Dispose();
+
+            Dispose();
+
+            // Dateien umbennen
+            if (result == DialogResult.Yes)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    File.Move(files[i], (string)lstNew.Items[i]);
+                }
+            }
+        }
+    }
+}
